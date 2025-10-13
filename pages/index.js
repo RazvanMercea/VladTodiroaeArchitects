56c06e60f028daddd40405b1bd7bf7a1ac0d159d
alexandra-popa27 <alexandradianapopa@yahoo.com>
2025-10-13 22:21:32 +0300

    irebase connection

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-export default function Home() {
-  return (
-    <div>Hello!</div>
-  );
+import { useEffect } from "react";
+import { db } from "@/lib/firebase";
+
+export default function HomePage() {
+  useEffect(() => {
+    console.log("Firebase connected:", db);
+  }, []);
+
+  return <h1>Vlad Todiroaie Architects</h1>;
 }
