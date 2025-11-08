3c141016d50205ad0d15593c24c63670c762b810
alexandra-popa27 <alexandradianapopa@yahoo.com>
2025-11-08 04:35:58 +0200

    fix

--- pages/project-details.js
+++ pages/project-details.js
@@ -353,7 +353,7 @@ const ProjectDetail = () => {
                 ) : (
                   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     {similarProjects.map((p) => (
-                      <ProjectCard key={p.id} project={p} />
+                      <ProjectCard key={p.id} project={p} countRooms={countRooms}/>
                     ))}
                   </div>
                 )}
